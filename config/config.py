36439cfde0224db2cb13897c6bec5177769baeb7
lamb-does-code <alessandro.lamberti98@gmail.com>
2021-12-06 19:55:47 +0100

    weights name

--- config/config.py
+++ config/config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DEVICE = getenv('DEVICE')
-MODEL_PATH = "weights/yolov5_m.pt"
+MODEL_PATH = "weights/yolo_detector.pt"
 DETECTION_THRESHOLD = 0.5
 MIN_ROI = 600
